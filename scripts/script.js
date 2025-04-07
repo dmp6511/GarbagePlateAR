0d5c6b266fc924962e426e64f3085e8f1c75bebf
De'Andre Perry <dmp6511@rit.edu>
2025-04-07 11:59:13 -0400

    script linked, to-do listed written

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Scripts for AR.js
+
+// TO DO: Make the model viewable and clickable
+// TO DO: Add some animation to the model
+// TO DO: Add a popup with information about the model
+// TO DO: Add a button to get an external link to the restaurant's site
+// TO DO: Add music and/or sounds to the experience to make it more immersive
