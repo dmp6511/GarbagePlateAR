958468dbc0a17873624af9d50622f4793604af9d
deandreperry <dmp6511@rit.edu>
2025-04-11 14:49:51 -0400

    plate should be clickable

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
 // Scripts for AR.js
 
-// TO DO: Make the model viewable and clickable
+// TO DO: Make the model clickable
+window.addEventListener('DOMContentLoaded', () => {
+    const plate = document.querySelector("#gbPlate");
+
+    plate.addEventListener('click', () => {
+        alert("You clicked the plate!");
+    });
+});
 // TO DO: Add some animation to the model
 // TO DO: Add a popup with information about the model
 // TO DO: Add a button to get an external link to the restaurant's site
